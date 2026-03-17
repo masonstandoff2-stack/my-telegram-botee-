ff02c55f14fbbd284e7440e8f09f007065409036
masonstandoff2-stack <masonstandoff2@gmail.com>
2026-03-17 23:08:02 +0300

    Update bot.py

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 TOKEN = "8356262671:AAFpw2GxPp7_DAnFDPX45cn6lr3f3AXUffY"
-FILE_ID = "BQACAgEAAxkBAAIojmmxZ_EAAcyDgSDGiYU-WrIOGKJdugACYgcAAn8oiUXhNctoXPZ8vDoE"
+FILE_ID = "BQACAgEAAxkBAAIqQGm5tGaw1zCgdkOp_B7C0SgbvRfIAAImBgACd13RRaOGA26Mci6pOgQ"
 
 # Каналы
 CHANNEL1_ID = "-1003318734165"
